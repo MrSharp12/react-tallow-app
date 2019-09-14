4100a8a0032692e41ccb0226d5c3806f59f0a4bb
The Sharps <Sharp@Thes-Air.attlocal.net>
2019-09-13 22:38:20 -0400

    changes front page info

--- src/components/HomePage/Info.js
+++ src/components/HomePage/Info.js
@@ -1,34 +1,23 @@
 import React from 'react';
 import Title from '../Title';
-import mereIMG from '../../images/mereRotate.jpg';
 
 const Info = () => {
     return (
         <section className="py-5">
             <div className="container">
                 <div className="row">
-                    <div className="col-10 mx-auto col-md-5 my-5">
-                        <img 
-                        src={mereIMG} 
-                        className="img-fluid img-thumbnail" 
-                        alt="about"
-                        style={{ background: "var(--darkGrey"}} 
-                        />
-                    </div>
                     <div className="col-10 mx-auto col-md-6 my-3">
-                    <Title title="About" center />
-                    <p className="text-lead text-muted my-3">Having skin issues most of her life, 
-                    Meredith was intrigued to learn that tallow balm could soothe and treat a variety of skin ailments.
-                    She had experimented with balm making in the past.
-                    When she found out that her friend's son had severe eczema, 
-                    she began to design different tallow balm blends to help his skin.</p>
-                    <p className="text-lead text-muted my-3">Sharing her product with others, she saw first hand the benefits of tallow based skin products.
-                    And now she can't wait to share them with you.</p>
+                        <Title title="About" center />
+                        <p className="text-lead text-muted my-3">Tallow balm is a unique skincare product made
+                                                                 from Simpson Family Farm's beyond organic, 100% grass-fed cows.
+                                                                 Used for centuries to nourish and soothe a variety of skin conditions,
+                                                                 tallow balm works well on dry chapped skin, rashes, burns, itching, wrinkles,
+                                                             acne, eczema, psoriasis, razor burn, and more.</p>
                     </div>
                 </div>
             </div>
-            
-            
+
+
         </section>
     )
 };
